13940f4741b1a2853386451172c79087cc4bec04
Negm-elgaan <ohossam962@gmail.com>
2025-03-02 14:27:59 +0200

    EDA

--- Exploratory Data Analysis in Python.py
+++ Exploratory Data Analysis in Python.py
@@ -160,3 +160,220 @@ planes["Duration_Category"] = np.select(conditions,
 # Plot the counts of each category
 sns.countplot(data=planes, x="Duration_Category")
 plt.show()
+###################
+# Preview the column
+print(planes['Duration'].head())
+#######################
+# Preview the column
+print(planes["Duration"].head())
+
+# Remove the string character
+planes["Duration"] = planes['Duration'].str.replace('h','')
+###############################
+# Preview the column
+print(planes["Duration"].head())
+
+# Remove the string character
+planes["Duration"] = planes["Duration"].str.replace("h", "")
+
+# Convert to float data type
+planes["Duration"] = planes['Duration'].astype(float)
+################################
+# Preview the column
+print(planes["Duration"].head())
+
+# Remove the string character
+planes["Duration"] = planes["Duration"].str.replace("h", "")
+
+# Convert to float data type
+planes["Duration"] = planes["Duration"].astype(float)
+
+# Plot a histogram
+sns.histplot(x = 'Duration' , data = planes)
+plt.show()
+######################
+# Price standard deviation by Airline
+planes["airline_price_st_dev"] = planes.groupby("Airline")["Price"].transform(lambda x: x.std())
+
+print(planes[["Airline", "airline_price_st_dev"]].value_counts())
+#########################
+# Median Duration by Airline
+planes["airline_median_duration"] = planes.groupby("Airline")["Duration"].transform(lambda x: x.median())
+
+print(planes[["Airline","airline_median_duration"]].value_counts())
+############################
+# Mean Price by Destination
+planes["price_destination_mean"] = planes.groupby("Destination")["Price"].transform(lambda x: x.mean())
+
+print(planes[["Destination","price_destination_mean"]].value_counts())
+######################
+# Plot a histogram of flight prices
+sns.histplot(data=planes, x='Price')
+plt.show()
+###############
+# Plot a histogram of flight prices
+sns.histplot(data=planes, x="Price")
+plt.show()
+
+# Display descriptive statistics for flight duration
+print(planes['Duration'].describe())
+####################
+# Find the 75th and 25th percentiles
+price_seventy_fifth = planes["Price"].quantile(0.75)
+price_twenty_fifth = planes["Price"].quantile(0.25)
+#######################
+# Find the 75th and 25th percentiles
+price_seventy_fifth = planes["Price"].quantile(0.75)
+price_twenty_fifth = planes["Price"].quantile(0.25)
+
+# Calculate iqr
+prices_iqr = price_seventy_fifth - price_twenty_fifth
+#########################
+# Find the 75th and 25th percentiles
+price_seventy_fifth = planes["Price"].quantile(0.75)
+price_twenty_fifth = planes["Price"].quantile(0.25)
+
+# Calculate iqr
+prices_iqr = price_seventy_fifth - price_twenty_fifth
+
+# Calculate the thresholds
+upper = price_seventy_fifth + (1.5 * prices_iqr)
+lower = price_twenty_fifth - (1.5 * prices_iqr)
+####################################
+# Find the 75th and 25th percentiles
+price_seventy_fifth = planes["Price"].quantile(0.75)
+price_twenty_fifth = planes["Price"].quantile(0.25)
+
+# Calculate iqr
+prices_iqr = price_seventy_fifth - price_twenty_fifth
+
+# Calculate the thresholds
+upper = price_seventy_fifth + (1.5 * prices_iqr)
+lower = price_twenty_fifth - (1.5 * prices_iqr)
+
+# Subset the data
+planes = planes[(planes["Price"] > lower) & (planes["Price"] < upper)]
+
+print(planes["Price"].describe())
+#########################
+# Import divorce.csv, parsing the appropriate columns as dates in the import
+divorce = pd.read_csv('divorce.csv',parse_dates = ['divorce_date','dob_man','dob_woman','marriage_date'])
+print(divorce.dtypes)
+###########
+# Convert the marriage_date column to DateTime values
+divorce["marriage_date"] = pd.to_datetime(divorce["marriage_date"])
+###################
+# Define the marriage_year column
+divorce["marriage_year"] = divorce["marriage_date"].dt.year
+###################
+# Define the marriage_year column
+divorce["marriage_year"] = divorce["marriage_date"].dt.year
+
+# Create a line plot showing the average number of kids by year
+sns.lineplot(y = 'num_kids' , x = 'marriage_year' , data = divorce)
+plt.show()
+###############
+sns.pairplot(data = divorce)
+##################
+# Create the scatterplot
+sns.scatterplot(x = 'marriage_duration' , y = 'num_kids' , data = divorce)
+plt.show()
+########################
+# Create a pairplot for income_woman and marriage_duration
+sns.pairplot(data = divorce, vars=['income_woman', 'marriage_duration'])
+plt.show()
+##########################
+# Create the scatter plot
+sns.scatterplot(x = 'woman_age_marriage' , y = 'income_woman' , data = divorce , hue = 'education_woman')
+plt.show()
+####################
+# Create the KDE plot
+sns.kdeplot(x = 'marriage_duration' , hue = 'num_kids' , data = divorce)
+plt.show()
+###########################
+# Update the KDE plot so that marriage duration can't be smoothed too far
+sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", cut = 0)
+plt.show()
+###################
+# Update the KDE plot to show a cumulative distribution function
+sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", cut=0, cumulative = True)
+plt.show()
+################
+# Print the relative frequency of Job_Category
+print(salaries['Job_Category'].value_counts(normalize = True))
+########################
+# Cross-tabulate Company_Size and Experience
+print(pd.crosstab(salaries["Company_Size"], salaries["Experience"]))
+###############################
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"]))
+#######################
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"],
+           values =salaries["Salary_USD"], aggfunc="mean"))
+############################
+# Get the month of the response
+salaries["month"] = salaries["date_of_response"].dt.month
+
+# Extract the weekday of the response
+salaries["weekday"] = salaries["date_of_response"].dt.weekday
+
+# Create a heatmap
+sns.heatmap(salaries.corr(), annot=True)
+plt.show()
+##############################
+# Find the 25th percentile
+twenty_fifth = salaries["Salary_USD"].quantile(0.25)
+
+# Save the median
+salaries_median = salaries["Salary_USD"].median()
+
+# Gather the 75th percentile
+seventy_fifth = salaries["Salary_USD"].quantile(0.75)
+print(twenty_fifth, salaries_median, seventy_fifth)
+#######################
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+##########################
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth , salaries_median , seventy_fifth , salaries["Salary_USD"].max()]
+###############################
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth, salaries_median, seventy_fifth, salaries["Salary_USD"].max()]
+
+# Create salary_level
+salaries["salary_level"] = pd.cut(salaries["Salary_USD"],
+                                  bins=salary_ranges,
+                                  labels=salary_labels)
+##################################
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth, salaries_median, seventy_fifth, salaries["Salary_USD"].max()]
+
+# Create salary_level
+salaries["salary_level"] = pd.cut(salaries["Salary_USD"],
+                                  bins=salary_ranges,
+                                  labels=salary_labels)
+
+# Plot the count of salary levels at companies of different sizes
+sns.countplot(data=salaries, x="Company_Size", hue="salary_level")
+plt.show()
+##################
+# Filter for employees in the US or GB
+usa_and_gb = salaries[salaries["Employee_Location"].isin(["US", "GB"])]
+
+# Create a barplot of salaries by location
+sns.barplot(data=usa_and_gb, x="Employee_Location", y="Salary_USD")
+plt.show()
+#########################
+# Create a bar plot of salary versus company size, factoring in employment status
+sns.barplot(data=salaries, x="Company_Size", y="Salary_USD", hue="Employment_Status")
+plt.show()

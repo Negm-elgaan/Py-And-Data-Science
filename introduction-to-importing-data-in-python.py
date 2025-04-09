3697acc8100c0cdc75ab103577bf8570fcf5a225
Negm-elgaan <ohossam962@gmail.com>
2025-04-09 18:41:34 +0200

    Return of ALX SQL AND PYTHON

--- introduction-to-importing-data-in-python.py
+++ introduction-to-importing-data-in-python.py
@@ -237,3 +237,120 @@ plt.plot(data)
 plt.xlabel('time (min.)')
 plt.ylabel('normalized fluorescence (measure of expression)')
 plt.show()
+#####################
+# Import necessary module
+from sqlalchemy import create_engine
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+######################
+# Import necessary module
+from sqlalchemy import create_engine
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Save the table names to a list: table_names
+table_names = engine.table_names()
+
+# Print the table names to the shell
+print(table_names)
+#################
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine connection: con
+con = engine.connect()
+
+# Perform query: rs
+rs = con.execute('SELECT * FROM Album')
+
+# Save results of the query to DataFrame: df
+df = pd.DataFrame(rs)
+
+# Close connection
+con.close()
+
+# Print head of DataFrame df
+print(df.head())
+###########################
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT LastName , Title FROM Employee')
+    df = pd.DataFrame(rs.fetchmany(3))
+    df.columns = rs.keys()
+
+# Print the length of the DataFrame df
+print(len(df))
+
+# Print the head of the DataFrame df
+print(df.head())
+#############################
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect('sqlit:///Chinook.sqlite') as con:
+    rs = con.execute('SELECT * FROM Employee WHERE EmployeeId >= 6')
+    df = pd.DataFrame(rs)
+    df.columns = rs.keys()
+
+# Print the head of the DataFrame df
+print(df.head())
+########################
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM Album', engine)
+
+# Print head of DataFrame
+print(df.head())
+
+# Open engine in context manager and store query result in df1
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Album")
+    df1 = pd.DataFrame(rs.fetchall())
+    df1.columns = rs.keys()
+
+# Confirm that both methods yield the same result
+print(df.equals(df1))
+###############
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM Employee WHERE EmployeeId >= 6 ORDER BY BirthDate' , engine)
+
+# Print head of DataFrame
+print(df.head())
+##################
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect('sqlite:///Chinook.sqlite') as con:
+    rs = con.execute('SELECT Title , Name FROM Album AL INNER JOIN Artist AR ON AR.ArtistID == AL.ArtistID')
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
+#################
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query('SELECT * FROM PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId WHERE Milliseconds < 250000',engine)
+
+# Print head of DataFrame
+print(df.head())

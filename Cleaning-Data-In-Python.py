f8ff2d1946d3c52e2e6e1df99187f59d90269e7f
Negm-elgaan <ohossam962@gmail.com>
2025-04-15 18:52:20 +0200

    my man

--- Cleaning-Data-In-Python.py
+++ Cleaning-Data-In-Python.py
@@ -180,3 +180,216 @@ assert airlines_survey['survey_response'].str.len().min() > 40
 print(airlines_survey['survey_response'])
 print(resp_length)
 print(airlines_survey)
+#################################
+# Find values of acct_cur that are equal to 'euro'
+acct_eu = banking['acct_cur'] == 'euro'
+
+# Convert acct_amount where it is in euro to dollars
+banking.loc[banking['acct_cur'] == 'euro', 'acct_amount'] = banking.loc[banking['acct_cur'] == 'euro' , 'acct_amount'] * 1.1
+
+# Unify acct_cur column by changing 'euro' values to 'dollar'
+banking.loc[banking['acct_cur'] == 'euro', 'acct_cur'] = 'dollar'
+
+# Assert that only dollar currency remains
+assert banking['acct_cur'].unique() == 'dollar'
+##########################
+# Print the header of account_opened
+print(banking['account_opened'].head())
+##############################
+# Print the header of account_opened
+print(banking['account_opened'].head())
+
+# Convert account_opened to datetime
+banking['account_opened'] = pd.to_datetime(banking['account_opened'],
+                                           # Infer datetime format
+                                           infer_datetime_format = True,
+                                           # Return missing value for error
+                                           errors = 'coerce') 
+####################################
+# Print the header of account_opend
+print(banking['account_opened'].head())
+
+# Convert account_opened to datetime
+banking['account_opened'] = pd.to_datetime(banking['account_opened'],
+                                           # Infer datetime format
+                                           infer_datetime_format = True,
+                                           # Return missing value for error
+                                           errors = 'coerce') 
+
+# Get year of account opened
+banking['acct_year'] = banking['account_opened'].dt.strftime('%Y')
+
+# Print acct_year
+print(banking['acct_year'])
+#################################################
+# Store fund columns to sum against
+fund_columns = ['fund_A', 'fund_B', 'fund_C', 'fund_D']
+
+# Find rows where fund_columns row sum == inv_amount
+inv_equ = banking[fund_columns].sum(axis = 1) == banking['inv_amount']
+
+# Store consistent and inconsistent data
+consistent_inv = banking[inv_equ]
+inconsistent_inv = banking[~inv_equ]
+
+# Store consistent and inconsistent data
+print("Number of inconsistent investments: ", inconsistent_inv.shape[0])
+##############################
+# Store today's date and find ages
+today = dt.date.today()
+ages_manual = today.year - banking['birth_date'].dt.year
+
+# Find rows where age column == ages_manual
+age_equ = banking['age'] == ages_manual
+
+# Store consistent and inconsistent data
+consistent_ages = banking[age_equ]
+inconsistent_ages = banking[~age_equ]
+
+# Store consistent and inconsistent data
+print("Number of inconsistent ages: ", inconsistent_ages.shape[0])
+#############################
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+###########################
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+######################
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values('age')
+msno.matrix(banking_sorted)
+plt.show()
+##########################
+# Drop missing values of cust_id
+banking_fullid = banking.dropna(subset = ['cust_id'])
+
+# Compute estimated acct_amount
+acct_imp = banking_fullid['inv_amount'] * 5
+
+# Impute missing acct_amount with corresponding acct_imp
+banking_imputed = banking_fullid.fillna({'acct_amount':acct_imp})
+
+# Print number of missing values
+print(banking_imputed.isna().sum())
+##############################
+# Import process from thefuzz
+from thefuzz import process
+
+# Store the unique values of cuisine_type in unique_types
+unique_types = restaurants['cuisine_type'].unique()
+
+# Calculate similarity of 'asian' to all values of unique_types
+print(process.extract('asian', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'american' to all values of unique_types
+print(process.extract('american', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'italian' to all values of unique_types
+print(process.extract('italian', unique_types, limit = len(unique_types)))
+
+##########################
+# Inspect the unique values of the cuisine_type column
+print(restaurants['cuisine_type'].unique())
+##############################
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'] , limit = len(restaurants['cuisine_type']))
+
+# Inspect the first 5 matches
+print(matches[0:5])
+#############################
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+# Iterate through the list of matches to italian
+for match in matches:
+  # Check whether the similarity score is greater than or equal to 80
+  if match[1] >= 80:
+    # Select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+    restaurants.loc[restaurants['cuisine_type'] == match[0] , 'cuisine_type'] = 'italian' 
+##################################
+# Iterate through categories
+for cuisine in categories:  
+  # Create a list of matches, comparing cuisine with the cuisine_type column
+  matches = process.extract(cuisine, restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+  # Iterate through the list of matches
+  for match in matches:
+     # Check whether the similarity score is greater than or equal to 80
+    if match[1] >= 80:
+      # If it is, select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+      restaurants.loc[restaurants['cuisine_type'] == match[0]] = cuisine
+      
+# Inspect the final result
+print(restaurants['cuisine_type'].unique())
+############################################
+# Create an indexer and object and find possible pairs
+indexer = recordlinkage.Index()
+
+# Block pairing on cuisine_type
+indexer.block('cuisine_type')
+
+# Generate pairs
+pairs = indexer.index(restaurants, restaurants_new)
+################################
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+###################################
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Use the exact method for exact matches
+comp_cl.exact('city', 'city', label='city')
+comp_cl.exact('cuisine_type', 'cuisine_type', label='cuisine_type')
+
+# Use the string method for similar matches with a threshold
+comp_cl.string('rest_name', 'rest_name', label='name', threshold=0.8)
+#############################################
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Find exact matches on city, cuisine_types - 
+comp_cl.exact('city', 'city', label='city')
+comp_cl.exact('cuisine_type', 'cuisine_type', label='cuisine_type')
+
+# Find similar matches of rest_name
+comp_cl.string('rest_name', 'rest_name', label='name', threshold = 0.8) 
+
+# Get potential matches and print
+potential_matches = comp_cl.compute(pairs, restaurants, restaurants_new)
+print(potential_matches)
+####################################
+# Isolate potential matches with row sum >=3
+matches = potential_matches[potential_matches.sum(axis = 1) >= 3]
+
+# Get values of second column index of matches
+matching_indices = matches.index.get_level_values(1)
+
+# Subset restaurants_new based on non-duplicate values
+non_dup = restaurants_new[~restaurants_new.index.isin(matching_indices)]
+
+# Append non_dup to restaurants
+full_restaurants = restaurants.append(non_dup)
+print(full_restaurants)

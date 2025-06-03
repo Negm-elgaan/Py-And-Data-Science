14d9650967d64daa5cad7eb04ede0ccc4b757e63
Negm-elgaan <ohossam962@gmail.com>
2025-06-03 23:59:34 +0300

    First project with machine learning

--- supervised-learning-with-scikit-learn.py
+++ supervised-learning-with-scikit-learn.py
@@ -15,5 +15,52 @@ y_pred = knn.predict(X_new)
 
 # Print the predictions
 print("Predictions: {}".format(y_pred)) 
+##############################
+# Import the module
+from sklearn.model_selection import train_test_split
 
+X = churn_df.drop("churn", axis=1).values
+y = churn_df["churn"].values
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X , y , test_size = 0.2, random_state = 42, stratify = y)
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Fit the classifier to the training data
+knn.fit(X_train , y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+# Create neighbors
+neighbors = np.arange(1, 13)
+train_accuracies = {}
+test_accuracies = {}
+
+for neighbor in neighbors:
+  
+	# Set up a KNN Classifier
+	knn = KNeighborsClassifier(n_neighbors = neighbor)
+  
+	# Fit the model
+	knn.fit(X_train , y_train)
+  
+	# Compute accuracy
+	train_accuracies[neighbor] = knn.score(X_train, y_train)
+	test_accuracies[neighbor] = knn.score(X_test, y_test)
+print(neighbors, '\n', train_accuracies, '\n', test_accuracies)
+##################################
+import numpy as np
+
+# Create X from the radio column's values
+X = sales_df['radio'].values
+
+# Create y from the sales column's values
+y = sales_df['sales'].values
+
+# Reshape X
+X = X.reshape(-1,1)
+
+# Check the shape of the features and targets
+print(X.shape,y.shape)
 

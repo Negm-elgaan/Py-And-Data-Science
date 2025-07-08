fc19ef884be816a580153ce11e3692c68fdb22fb
Negm-elgaan <ohossam962@gmail.com>
2025-07-08 15:38:39 +0300

    New machine learning project

--- machine-learning-with-tree-based-models-in-python.py
+++ machine-learning-with-tree-based-models-in-python.py
@@ -35,3 +35,27 @@ clfs = [logreg, dt]
 
 # Review the decision regions of the two classifiers
 plot_labeled_decision_regions(X_test, y_test, clfs)
+###########################################
+# Import DecisionTreeClassifier from sklearn.tree
+from sklearn.tree import DecisionTreeClassifier
+
+# Instantiate dt_entropy, set 'entropy' as the information criterion
+dt_entropy = DecisionTreeClassifier(max_depth = 8 , criterion = 'entropy', random_state=1)
+
+# Fit dt_entropy to the training set
+dt_entropy.fit(X_train , y_train)
+#######################
+# Import accuracy_score from sklearn.metrics
+from sklearn.metrics import accuracy_score
+
+# Use dt_entropy to predict test set labels
+y_pred = dt_entropy.predict(X_test)
+
+# Evaluate accuracy_entropy
+accuracy_entropy = accuracy_score(y_pred , y_test)
+
+# Print accuracy_entropy
+print(f'Accuracy achieved by using entropy: {accuracy_entropy:.3f}')
+
+# Print accuracy_gini
+print(f'Accuracy achieved by using the gini index: {accuracy_gini:.3f}')

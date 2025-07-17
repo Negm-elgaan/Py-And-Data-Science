171fde95e38a16b30fcb31a5a42da39837ef21ab
Negm-elgaan <ohossam962@gmail.com>
2025-07-17 17:29:09 +0300

    final project of datacamp's accoicate data scientist in python

--- machine-learning-with-tree-based-models-in-python.py
+++ machine-learning-with-tree-based-models-in-python.py
@@ -59,3 +59,111 @@ print(f'Accuracy achieved by using entropy: {accuracy_entropy:.3f}')
 
 # Print accuracy_gini
 print(f'Accuracy achieved by using the gini index: {accuracy_gini:.3f}')
+########################################
+# Import DecisionTreeRegressor from sklearn.tree
+from sklearn.tree import DecisionTreeRegressor
+
+# Instantiate dt
+dt = DecisionTreeRegressor(max_depth = 8 , min_samples_leaf = 0.13 , random_state = 3)
+
+# Fit dt to the training set
+dt.fit(X_train , y_train)
+###########################
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute y_pred
+y_pred = dt.predict(X_test)
+
+# Compute mse_dt
+mse_dt = MSE(y_test , y_pred)
+
+# Compute rmse_dt
+rmse_dt = mse_dt ** (1/2)
+
+# Print rmse_dt
+print("Test set RMSE of dt: {:.2f}".format(rmse_dt))
+###################################
+# Predict test set labels 
+y_pred_lr = lr.predict(X_test)
+
+# Compute mse_lr
+mse_lr = MSE(y_test, y_pred_lr)
+
+# Compute rmse_lr
+rmse_lr = mse_lr ** (1/2)
+
+# Print rmse_lr
+print('Linear Regression test set RMSE: {:.2f}'.format(rmse_lr))
+
+# Print rmse_dt
+print('Regression Tree test set RMSE: {:.2f}'.format(rmse_dt))
+####################################################################################################################
+# Import train_test_split from sklearn.model_selection
+from sklearn.model_selection import train_test_split
+
+# Set SEED for reproducibility
+SEED = 1
+
+# Split the data into 70% train and 30% test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3 , random_state=SEED)
+
+# Instantiate a DecisionTreeRegressor dt
+dt = DecisionTreeRegressor(max_depth = 4 , min_samples_leaf = 0.26, random_state=SEED)
+###########################
+# Compute the array containing the 10-folds CV MSEs
+MSE_CV_scores = - cross_val_score(dt , X_train , y_train  , cv = 10, 
+                       scoring = 'neg_mean_squared_error',
+                       n_jobs=-1)
+
+# Compute the 10-folds CV RMSE
+RMSE_CV = (MSE_CV_scores.mean())**(1/2)
+
+# Print RMSE_CV
+print('CV RMSE: {:.2f}'.format(RMSE_CV))
+#####################################
+# Import mean_squared_error from sklearn.metrics as MSE
+from sklearn.metrics import mean_squared_error as mse
+
+# Fit dt to the training set
+dt.fit(X_train , y_train)
+
+# Predict the labels of the training set
+y_pred_train = dt.predict(X_train)
+
+# Evaluate the training set RMSE of dt
+RMSE_train = (mse(y_train , y_pred_train))**(1/2)
+
+# Print RMSE_train
+print('Train RMSE: {:.2f}'.format(RMSE_train))
+#####################
+# Set seed for reproducibility
+SEED = 1
+
+# Instantiate lr
+lr = LogisticRegression(random_state = SEED)
+
+# Instantiate knn
+knn = KNN(n_neighbors = 27)
+
+# Instantiate dt
+dt = DecisionTreeClassifier(min_samples_leaf = 0.13 , random_state = SEED)
+
+# Define the list classifiers
+classifiers = [('Logistic Regression', lr), ('K Nearest Neighbours', knn), ('Classification Tree', dt)]
+###############
+# Iterate over the pre-defined list of classifiers
+for clf_name, clf in classifiers:    
+ 
+    # Fit clf to the training set
+    clf.fit(X_train , y_train)    
+   
+    # Predict y_pred
+    y_pred = clf.predict(X_test)
+    
+    # Calculate accuracy
+    accuracy = accuracy_score(y_test , y_pred) 
+   
+    # Evaluate clf's accuracy on the test set
+    print('{:s} : {:.3f}'.format(clf_name, accuracy))
+#################

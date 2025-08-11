db6e95c2af16461ed6f87ce6eb00d3bb70c7066a
Negm-elgaan <ohossam962@gmail.com>
2025-08-12 02:11:25 +0300

    boom

--- intermediate-importing-data-in-python.py
+++ intermediate-importing-data-in-python.py
@@ -13,3 +13,91 @@ urlretrieve(url , 'winequality-red.csv')
 # Read file into a DataFrame and print its head
 df = pd.read_csv('winequality-red.csv', sep=';')
 print(df.head())
+############################
+# Import packages
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Assign url of file: url
+url = 'https://assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
+
+# Read file into a DataFrame: df
+df = pd.read_csv(url,';')
+
+# Print the head of the DataFrame
+print(df.head())
+
+# Plot first column of df
+df.iloc[:, 0].hist()
+plt.xlabel('fixed acidity (g(tartaric acid)/dm$^3$)')
+plt.ylabel('count')
+plt.show()
+#####################################
+# Import package
+import pandas as pd
+
+# Assign url of file: url
+url = 'https://assets.datacamp.com/course/importing_data_into_r/latitude.xls'
+
+# Read in all sheets of Excel file: xls
+xls = pd.read_excel(url,sheet_name = None)
+
+# Print the sheetnames to the shell
+print(xls.keys())
+
+# Print the head of the first sheet (using its name, NOT its index)
+print(xls['1700'].head())
+######################
+# Import packages
+from urllib.request import urlopen , Request
+
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+
+# This packages the request: request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Print the datatype of response
+print(type(response))
+
+# Be polite and close the response!
+response.close()
+##########################
+# Import packages
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+
+# This packages the request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Extract the response: html
+html = response.read()
+
+# Print the html
+print(html)
+
+# Be polite and close the response!
+response.close()
+################################
+# Import package
+import requests
+
+# Specify the url: url
+url = "http://www.datacamp.com/teach/documentation"
+
+# Packages the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response: text
+text = r.text
+
+# Print the html
+print(text)
---
+++ linear-classifiers-in-python.py
@@ -0,0 +1,25 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create and fit the model
+knn = KNeighborsClassifier(n_neighbors = 1)
+knn.fit(X_train , y_train)
+
+# Predict on the test features, print the results
+pred = knn.predict(X_test)[0]
+print("Prediction for test example 0:", pred)
+###################################
+from sklearn import datasets
+digits = datasets.load_digits()
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target)
+
+# Apply logistic regression and print scores
+lr = LogisticRegression()
+lr.fit(X_train , y_train)
+print(lr.score(X_train , y_train))
+print(lr.score(X_test , y_test))
+
+# Apply SVM and print scores
+svm = SVC()
+svm.fit(X_train , y_train)
+print(svm.score(X_train , y_train))
+print(svm.score(X_test , y_test))

f388e805aacb4f587f6da47ea17ceaf3a102b167
Negm-elgaan <125521344+Negm-elgaan@users.noreply.github.com>
2025-08-12 08:23:58 -0700

    Create Introduction_to_Tensorflow_in_Python.py

---
+++ Introduction_to_Tensorflow_in_Python.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+tensor1 = tf.ones((2,2,2,2))
+
+# Import constant from Tensor
+from tensorflow import constant 
+
+# Convert the credit_numpy array into a tensorflow constant
+credit_constant = constant(credit_numpy)
+
+# Print constant datatype
+print('\n The datatype is:', credit_constant.dtype)
+
+# Print constant shape
+print('\n The shape is:', credit_constant.shape)
+# Define the 1-dimensional variable A1
+A1 = Variable([1, 2, 3, 4])
+
+# Print the variable A1
+print('\n A1: ', A1)
+
+# Convert A1 to a numpy array and assign it to B1
+B1 = A1.numpy()
+
+# Print B1
+print('\n B1: ', B1)

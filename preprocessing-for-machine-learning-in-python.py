f93a71d441f5b22ea61ae04542fa8af41cbca619
Negm-elgaan <ohossam962@gmail.com>
2025-09-02 08:07:04 +0300

    Blahblah

--- preprocessing-for-machine-learning-in-python.py
+++ preprocessing-for-machine-learning-in-python.py
@@ -31,3 +31,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = y, random_s
 
 # Print the category_desc counts from y_train
 print(y_train['category_desc'].value_counts())
+#########################################
+# Split the dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X , y , stratify = y, random_state=42)
+
+knn = KNeighborsClassifier()
+
+# Fit the knn model to the training data
+knn.fit(X_train , y_train)
+
+# Score the model on the test data
+print(knn.score(X_test,y_test))

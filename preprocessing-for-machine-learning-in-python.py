d502d475001725e24b60b19d41b60be0a85f2051
Negm-elgaan <ohossam962@gmail.com>
2025-10-14 02:22:02 +0300

    New log

--- preprocessing-for-machine-learning-in-python.py
+++ preprocessing-for-machine-learning-in-python.py
@@ -166,3 +166,43 @@ print(wine.corr())
 wine = wine.drop('Flavanoids' , axis = 1)
 
 print(wine.head())
+##################################
+# Add in the rest of the arguments
+def return_weights(vocab , original_vocab , vector , vector_index , top_n):
+    zipped = dict(zip(vector[vector_index].indices, vector[vector_index].data))
+    
+    # Transform that zipped dict into a series
+    zipped_series = pd.Series({vocab[i]:zipped[i] for i in vector[vector_index].indices})
+    
+    # Sort the series to pull out the top n weighted words
+    zipped_index = zipped_series.sort_values(ascending=False)[:top_n].index
+    return [original_vocab[i] for i in zipped_index]
+
+# Print out the weighted words
+print(return_weights(vocab, tfidf_vec.vocabulary_ , text_tfidf, 8 , 3))
+#########################
+def words_to_filter(vocab, original_vocab, vector, top_n):
+    filter_list = []
+    for i in range(0, vector.shape[0]):
+    
+        # Call the return_weights function and extend filter_list
+        filtered = return_weights(vocab, original_vocab, vector, i, top_n)
+        filter_list.extend(filtered)
+        
+    # Return the list in a set, so we don't get duplicate word indices
+    return set(filter_list)
+
+# Call the function to get the list of word indices
+filtered_words = words_to_filter(vocab , tfidf_vec.vocabulary_ , text_tfidf , 3)
+
+# Filter the columns in text_tfidf to only those in filtered_words
+filtered_text = text_tfidf[:, list(filtered_words)]
+###################################
+# Split the dataset according to the class distribution of category_desc
+X_train, X_test, y_train, y_test = train_test_split(filtered_text.toarray(), y , stratify = y, random_state=42)
+
+# Fit the model to the training data
+nb.fit(X_train , y_train)
+
+# Print out the model's accuracy
+print(nb.score(X_test , y_test))

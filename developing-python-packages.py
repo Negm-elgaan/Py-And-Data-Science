ff949e2d33b4ffa0b0cd0f61191e129bd0655c36
Negm-elgaan <ohossam962@gmail.com>
2025-11-05 03:37:46 +0200

    Turin Turiamber

--- developing-python-packages.py
+++ developing-python-packages.py
@@ -193,3 +193,79 @@ def convert_unit(x, from_unit, to_unit):
 
     return value
 
+############################
+# This is the top level __init__.py file
+
+# Import the length subpackage
+from . import length
+###################
+# This is the __init__.py file for the impyrial/length subpackage
+
+# Import the convert_unit function from the api.py module
+from ..api import convert_unit
+##########################
+# Import required functions
+from setuptools import setup , find_packages
+
+# Call setup function
+setup(
+    author = "NEGM_ELG3AN" ,
+    description = "A package for converting impyrial lengths and weights." ,
+    name = "impyrial" ,
+    packages = find_packages( include = ['impyrial' , 'impyrial.*'] ) ,
+    version = "0.1.0" ,
+)
+#################
+pip install -e .
+###########################
+"""Conversions between ounces and larger imperial weight units"""
+OUNCES_PER_POUND = 16.0  # 16 ounces in a pound
+OUNCES_PER_STONE = OUNCES_PER_POUND * 14.0  # 14 pounds in a stone
+
+UNITS = ("oz", "lb", "st")
+
+
+def ounces_to_pounds(x, reverse=False):
+    """Convert weights between ounces and pounds.
+
+    Parameters
+    ----------
+    x : array_like
+        Weights in pounds.
+    reverse : bool, optional
+        If this is set to true this function converts from pounds to ounces
+        instead of the default behaviour of ounces to pounds.
+
+    Returns
+    -------
+    ndarray
+        An array of converted weights with the same shape as `x`. If `x` is a
+        0-d array, then a scalar is returned.
+    """
+    if reverse:
+        return x * OUNCES_PER_POUND
+    else:
+        return x / OUNCES_PER_POUND
+
+
+def ounces_to_stone(x, reverse=False):
+    """Convert weights between ounces and stone.
+
+    Parameters
+    ----------
+    x : array_like
+        Weights in stone.
+    reverse : bool, optional
+        If this is set to true this function converts from stone to ounces
+        instead of the default behaviour of ounces to stone.
+
+    Returns
+    -------
+    ndarray
+        An array of converted weights with the same shape as `x`. If `x` is a
+        0-d array, then a scalar is returned.
+    """
+    if reverse:
+        return x * OUNCES_PER_STONE
+    else:
+        return x / OUNCES_PER_STONE

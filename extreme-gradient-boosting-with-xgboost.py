59c0812498d8c11256a24ad31538ff0b4e9a012a
Negm-elgaan <ohossam962@gmail.com>
2025-11-16 00:58:24 +0200

    FINGOLFIN

--- extreme-gradient-boosting-with-xgboost.py
+++ extreme-gradient-boosting-with-xgboost.py
@@ -282,3 +282,71 @@ for curr_val in max_depths:
 
 # Print the resultant DataFrame
 print(pd.DataFrame(list(zip(max_depths, best_rmse)),columns=["max_depth","best_rmse"]))
+#################################
+# Create your housing DMatrix
+housing_dmatrix = xgb.DMatrix(data=X,label=y)
+
+# Create the parameter dictionary
+params={"objective":"reg:squarederror","max_depth":3}
+
+# Create list of hyperparameter values: colsample_bytree_vals
+colsample_bytree_vals = [0.1 , 0.5 , 0.8 , 1]
+best_rmse = []
+
+# Systematically vary the hyperparameter value 
+for curr_val in colsample_bytree_vals:
+
+    params['colsample_bytree'] = curr_val 
+    
+    # Perform cross-validation
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=2,
+                 num_boost_round=10, early_stopping_rounds=5,
+                 metrics="rmse", as_pandas=True, seed=123)
+    
+    # Append the final round rmse to best_rmse
+    best_rmse.append(cv_results["test-rmse-mean"].tail().values[-1])
+
+# Print the resultant DataFrame
+print(pd.DataFrame(list(zip(colsample_bytree_vals, best_rmse)), columns=["colsample_bytree","best_rmse"]))
+##################################
+# Create the parameter grid: gbm_param_grid
+gbm_param_grid = {
+    'colsample_bytree': [0.3 , 0.7],
+    'n_estimators': [50],
+    'max_depth': [2,5]
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor()
+
+# Perform grid search: grid_mse
+grid_mse = GridSearchCV(param_grid = gbm_param_grid, estimator = gbm , scoring = "neg_mean_squared_error" , cv = 4 , verbose = 1)
+
+
+# Fit grid_mse to the data
+grid_mse.fit(X,y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", grid_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(grid_mse.best_score_)))
+#####################################
+from sklearn.model_selection import RandomizedSearchCV as RSCV
+# Create the parameter grid: gbm_param_grid 
+gbm_param_grid = {
+    'n_estimators': [25] ,
+    'max_depth': range(2 , 12)
+}
+
+# Instantiate the regressor: gbm
+gbm = xgb.XGBRegressor(n_estimators=10)
+
+# Perform random search: grid_mse
+randomized_mse = RSCV(param_distributions = gbm_param_grid , estimator = gbm , scoring = 'neg_mean_squared_error' , n_iter = 5 , cv = 4 , verbose = 1)
+
+
+# Fit randomized_mse to the data
+randomized_mse.fit(X , y)
+
+# Print the best parameters and lowest RMSE
+print("Best parameters found: ", randomized_mse.best_params_)
+print("Lowest RMSE found: ", np.sqrt(np.abs(randomized_mse.best_score_)))

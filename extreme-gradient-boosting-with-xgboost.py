aa03c407bd3f0d5cd55d66d874b30568eb6f53a4
Negm-elgaan <ohossam962@gmail.com>
2025-11-17 13:31:02 +0200

    here we go

--- extreme-gradient-boosting-with-xgboost.py
+++ extreme-gradient-boosting-with-xgboost.py
@@ -350,3 +350,67 @@ randomized_mse.fit(X , y)
 # Print the best parameters and lowest RMSE
 print("Best parameters found: ", randomized_mse.best_params_)
 print("Lowest RMSE found: ", np.sqrt(np.abs(randomized_mse.best_score_)))
+######################
+df.info()
+df.describe()
+df.isna().sum()
+#####################
+# Import LabelEncoder
+from sklearn.preprocessing import LabelEncoder
+
+# Fill missing values with 0
+df.LotFrontage = df.LotFrontage.fillna(0)
+
+# Create a boolean mask for categorical columns
+categorical_mask = (df.dtypes == object)
+
+# Get list of categorical column names
+categorical_columns = df.columns[categorical_mask].tolist()
+
+# Print the head of the categorical columns
+print(df[categorical_columns].head())
+
+# Create LabelEncoder object: le
+le = LabelEncoder()
+
+# Apply LabelEncoder to categorical columns
+df[categorical_columns] = df[categorical_columns].apply(lambda x: le.fit_transform(x))
+
+# Print the head of the LabelEncoded categorical columns
+print(df[categorical_columns].head())
+##################################
+# Import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+# Create OneHotEncoder: ohe
+ohe = OneHotEncoder(sparse = False)
+
+# Apply OneHotEncoder to categorical columns - output is no longer a dataframe: df_encoded
+df_encoded = ohe.fit_transform(df)
+
+# Print first 5 rows of the resulting dataset - again, this will no longer be a pandas dataframe
+print(df_encoded[:5, :])
+
+# Print the shape of the original DataFrame
+print(df.shape)
+
+# Print the shape of the transformed array
+print(df_encoded.shape)
+########################
+# Import DictVectorizer
+from sklearn.feature_extraction import DictVectorizer
+
+# Convert df into a dictionary: df_dict
+df_dict = df.to_dict('records')
+
+# Create the DictVectorizer object: dv
+dv = DictVectorizer(sparse = False)
+
+# Apply dv on df: df_encoded
+df_encoded = dv.fit_transform(df_dict)
+
+# Print the resulting first five rows
+print(df_encoded[:5,:])
+
+# Print the vocabulary
+print(dv.vocabulary_)

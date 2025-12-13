87cbdc96d2d604fc1cf385b2034df1441b4ab010
Negm-elgaan <ohossam962@gmail.com>
2025-12-13 21:04:25 +0200

    We are back

--- cluster-analysis-in-python.py
+++ cluster-analysis-in-python.py
@@ -19,3 +19,16 @@ df['cluster_labels'] = fcluster(Z, 2, criterion='maxclust')
 # Plot the points with seaborn
 sns.scatterplot(x = 'x' , y = 'y' , hue = 'cluster_labels', data=df)
 plt.show()
+############################
+# Import kmeans and vq functions
+from scipy.cluster.vq import kmeans , vq
+
+# Compute cluster centers
+centroids,_ = kmeans(df ,  2)
+
+# Assign cluster labels
+df['cluster_labels'], _ = vq(df  , centroids)
+
+# Plot the points with seaborn
+sns.scatterplot(x='x', y='y', hue='cluster_labels', data=df)
+plt.show()

050d81fa12250e1f20ea8a6027b92aab69e43862
Negm-elgaan <ohossam962@gmail.com>
2025-12-25 17:37:45 +0200

    JAX AND TF

--- Introduction_to_Tensorflow_in_Python.py
+++ Introduction_to_Tensorflow_in_Python.py
@@ -289,3 +289,34 @@ predictions = keras.layers.Dense(1 , activation = 'sigmoid')(dense2)
 print('\n shape of dense1: ', dense1.shape)
 print('\n shape of dense2: ', dense2.shape)
 print('\n shape of predictions: ', predictions.shape)
+#############################
+# Construct input layer from features
+inputs = constant(bill_amounts , float32)
+
+# Define first dense layer
+dense1 = keras.layers.Dense(3 , activation = 'relu')(inputs)
+
+# Define second dense layer
+dense2 = keras.layers.Dense(2 , activation = 'relu')(dense1)
+
+# Define output layer
+outputs = keras.layers.Dense(1 , activation = 'sigmoid')(dense2)
+
+# Print error for first five examples
+error = default[:5] - outputs.numpy()[:5]
+print(error)
+##############################
+# Construct input layer from borrower features
+inputs = constant(borrower_features , float32)
+
+# Define first dense layer
+dense1 = keras.layers.Dense(10 , activation = 'sigmoid')(inputs)
+
+# Define second dense layer
+dense2 = keras.layers.Dense(8 , activation = 'relu')(dense1)
+
+# Define output layer
+outputs = keras.layers.Dense(6 , activation = 'softmax')(dense2)
+
+# Print first five predictions
+print(outputs.numpy()[:5])
---
+++ simplefunctionusingJax.py
@@ -0,0 +1,5 @@
+import jax as JAX
+import jax.numpy as JNP
+
+def Fun(x):
+	return JNP.tan(x) + x ** 3

c3276ed5e4d0bc6ab50c957d735c00f7134efbf2
Negm-elgaan <ohossam962@gmail.com>
2026-01-17 14:06:33 +0200

    Small progress on NLP

--- natural-language-processing-nlp-in-python.py
+++ natural-language-processing-nlp-in-python.py
@@ -11,3 +11,25 @@ However, many investors are optimistic about future growth.
 # Tokenize the text into sentences
 sentences = nltk.sent_tokenize(text)
 print(sentences)
+######################
+feedback = "I reached out to support and got a helpful response within minutes!!! Very #impressed"
+
+# Tokenize the text
+tokens = word_tokenize(feedback)
+
+# Get the list of English stop words
+stop_words = stopwords.words('english')
+
+# Remove stop words 
+filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+
+print(filtered_tokens)
+#############################
+import string
+
+filtered_tokens = ['reached', 'support', 'got', 'helpful', 'response', 'within', 'minutes', '!', '!', '!', '#', 'impressed']
+
+# Remove punctuation
+clean_tokens = [word for word in filtered_tokens if word not in string.punctuation]
+
+print(clean_tokens)

e29a5e2025b42fed52e373432b5170b47ebb9fbc
Negm-elgaan <ohossam962@gmail.com>
2026-01-31 20:40:23 +0200

    Now in final chapter of tensorflow

--- Introduction_to_Tensorflow_in_Python.py
+++ Introduction_to_Tensorflow_in_Python.py
@@ -390,3 +390,50 @@ model_predictions = model(w1, b1, w2, b2, test_features)
 
 # Construct the confusion matrix
 confusion_matrix(test_targets, model_predictions)
+################################
+# Define a Keras sequential model
+from tensorflow import keras as ks
+
+model = ks.Sequential()
+
+# Define the first dense layer
+model.add(keras.layers.Dense(16 , activation='relu', input_shape=(784,)))
+
+# Define the second dense layer
+model.add(keras.layers.Dense(8 , activation = 'relu'))
+
+# Define the output layer
+model.add(keras.layers.Dense(4 , activation = 'softmax'))
+
+# Print the model architecture
+print(model.summary())
+##############################################
+# Define the first dense layer
+model.add(keras.layers.Dense(16 , activation = 'sigmoid' , input_shape =(784,)))
+
+# Apply dropout to the first layer's output
+model.add(keras.layers.Dropout(0.25))
+
+# Define the output layer
+model.add(keras.layers.Dense(4 , activation = 'softmax'))
+
+# Compile the model
+model.compile('adam', loss = 'categorical_crossentropy')
+
+# Print a model summary
+print(model.summary())
+##################################
+# For model 1, pass the input layer to layer 1 and layer 1 to layer 2
+m1_layer1 = keras.layers.Dense(12, activation='sigmoid')(m1_inputs)
+m1_layer2 = keras.layers.Dense(4, activation='softmax')(m1_layer1)
+
+# For model 2, pass the input layer to layer 1 and layer 1 to layer 2
+m2_layer1 = keras.layers.Dense(12, activation='relu')(m2_inputs)
+m2_layer2 = keras.layers.Dense(4, activation='softmax')(m2_layer1)
+
+# Merge model outputs and define a functional model
+merged = keras.layers.add([m1_layer2, m2_layer2])
+model = keras.Model(inputs=[m1_inputs, m2_inputs], outputs = merged)
+
+# Print a model summary
+print(model.summary())

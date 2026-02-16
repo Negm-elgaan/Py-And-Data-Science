e8e45bed7d30602ce7939fc1954381ab4007c4e5
Negm-elgaan <ohossam962@gmail.com>
2026-02-16 23:48:16 +0200

    More clusters

--- cluster-analysis-in-python.py
+++ cluster-analysis-in-python.py
@@ -104,3 +104,17 @@ comic_con['cluster_labels'] = FC(distance_matrix , 2 , criterion='maxclust')
 sns.scatterplot(x='x_scaled', y='y_scaled', 
                 hue='cluster_labels', data = comic_con)
 plt.show()
+###########################
+# Import the fcluster and linkage functions
+from scipy.cluster.hierarchy import fcluster , linkage
+
+# Use the linkage() function
+distance_matrix = linkage(comic_con[['x_scaled' , 'y_scaled']], method = 'single', metric = 'euclidean')
+
+# Assign cluster labels
+comic_con['cluster_labels'] = fcluster(distance_matrix, 2 , criterion = 'maxclust')
+
+# Plot clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', 
+                hue='cluster_labels', data = comic_con)
+plt.show()

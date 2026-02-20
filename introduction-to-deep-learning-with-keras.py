6f3a9e30664bc11a34ab394d2b2f9a23cc8c9530
Negm-elgaan <ohossam962@gmail.com>
2026-02-20 23:18:26 +0200

    nahadhdhdhdhd

--- introduction-to-deep-learning-with-keras.py
+++ introduction-to-deep-learning-with-keras.py
@@ -73,3 +73,43 @@ eighty_min_orbit = model.predict(np.arange(-40, 41))
 
 # Plot the eighty minute orbit 
 plot_orbit(eighty_min_orbit)
+#####################################
+# Import seaborn
+import seaborn as sns
+
+# Use pairplot and set the hue to be our class column
+sns.pairplot(banknotes, hue = 'class') 
+
+# Show the plot
+plt.show()
+
+# Describe the data
+print('Dataset stats: \n', banknotes.describe())
+
+# Count the number of observations per class
+print('Observations per class: \n', banknotes['class'].value_counts())
+##############
+# Import the sequential model and dense layer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# Create a sequential model
+model = Sequential()
+
+# Add a dense layer 
+model.add(Dense(1, input_shape=(4,), activation='sigmoid'))
+
+# Compile your model
+model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# Display a summary of your model
+model.summary()
+######################
+# Train your model for 20 epochs
+model.fit(X_train , y_train , epochs = 20)
+
+# Evaluate your model accuracy on the test set
+accuracy = model.evaluate(X_test , y_test)[1]
+
+# Print accuracy
+print('Accuracy:', accuracy)

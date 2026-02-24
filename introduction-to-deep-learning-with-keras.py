d7e8e69b678feaeaeba22b7cab3cf073937269d1
Negm-elgaan <ohossam962@gmail.com>
2026-02-25 00:22:04 +0200

    autoencoders

--- introduction-to-deep-learning-with-keras.py
+++ introduction-to-deep-learning-with-keras.py
@@ -187,3 +187,46 @@ preds_chosen = [np.argmax(pred) for pred in preds]
 print("{:10} | {}".format('Rounded Model Predictions','True labels'))
 for i,pred in enumerate(preds_chosen):
   print("{:25} | {}".format(pred,competitors_small_test[i]))
+##################################
+# Import tensorflow.keras backend
+import tensorflow.keras.backend as K
+
+# Input tensor from the 1st layer of the model
+inp = model.layers[0].input
+
+# Output tensor from the 1st layer of the model
+out = model.layers[0].output
+
+# Define a function from inputs to outputs
+inp_to_out = K.function([inp], [out])
+
+# Print the results of passing X_test through the 1st layer
+print(inp_to_out([X_test]))
+#########################################
+for i in range(0, 21):
+  	# Train model for 1 epoch
+    h = model.fit(X_train, y_train, batch_size = 16, epochs = 1, verbose = 0)
+    if i%4==0: 
+      # Get the output of the first layer
+      layer_output = inp_to_out([X_test])[0]
+      
+      # Evaluate model accuracy for this epoch
+      test_accuracy = model.evaluate(X_test , y_test)[1] 
+      
+      # Plot 1st vs 2nd neuron output
+      plot()
+########################
+# Start with a sequential model
+autoencoder = Sequential()
+
+# Add a dense layer with input the original image pixels and neurons the encoded representation
+autoencoder.add(Dense(32 , input_shape = (784,) , activation = 'relu'))
+
+# Add an output layer with as many neurons as the orginal image pixels
+autoencoder.add(Dense(784 , activation = 'sigmoid'))
+
+# Compile your model with adadelta
+autoencoder.compile(optimizer = 'adadelta' , loss = 'binary_crossentropy' , metrics = ['accuracy'])
+
+# Summarize your model structure
+autoencoder.summary()

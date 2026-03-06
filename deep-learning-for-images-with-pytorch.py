50368a7900edd79676a12bbbe499e1b478b4d063
Negm-elgaan <ohossam962@gmail.com>
2026-03-06 11:32:14 +0200

    final of pytorch for images

--- deep-learning-for-images-with-pytorch.py
+++ deep-learning-for-images-with-pytorch.py
@@ -427,3 +427,118 @@ for mask in instance_masks:
 plt.imshow(panoptic_mask.squeeze(0))
 plt.axis("off")
 plt.show()
+#########################################
+class DCGenerator(nn.Module):
+    def __init__(self, in_dim, kernel_size=4, stride=2):
+        super(DCGenerator, self).__init__()
+        self.in_dim = in_dim
+        self.gen = nn.Sequential(
+            dc_gen_block(in_dim, 1024, kernel_size, stride),
+            dc_gen_block(1024, 512, kernel_size, stride),
+            # Add last generator block
+            dc_gen_block(512 , 256 , kernel_size , stride),
+            # Add transposed convolution
+            ConvTranspose2d(256, 3, kernel_size, stride=stride),
+            # Add tanh activation
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = x.view(len(x), self.in_dim, 1, 1)
+        return self.gen(x)
+##########################
+class DCDiscriminator(nn.Module):
+    def __init__(self, kernel_size=4, stride=2):
+        super(DCDiscriminator, self).__init__()
+        self.disc = nn.Sequential(
+          	# Add first discriminator block
+            dc_disc_block(3, 512, kernel_size, stride),
+            dc_disc_block(512, 1024, kernel_size, stride),
+          	# Add a convolution
+            nn.Conv2d(1024, 1, kernel_size, stride=stride),
+        )
+
+    def forward(self, x):
+        # Pass input through sequential block
+        x = self.disc(x)
+        return x.view(len(x), -1)
+##############################
+def gen_loss(gen, disc, criterion, num_images, z_dim):
+    # Define random noise
+    noise = torch.randn(num_images, z_dim)
+    # Generate fake image
+    fake = gen(noise)
+    # Get discriminator's prediction on the fake image
+    disc_pred = disc(fake)
+    # Compute generator loss
+    criterion = nn.BCEWithLogitsLoss()
+    gen_loss = criterion(disc_pred , torch.ones_like(disc_pred))
+    return gen_loss
+###################################
+def disc_loss(gen, disc, real, num_images, z_dim):
+    criterion = nn.BCEWithLogitsLoss()
+    noise = torch.randn(num_images, z_dim)
+    fake = gen(noise)
+    # Get discriminator's predictions for fake images
+    disc_pred_fake = disc(fake)
+    # Calculate the fake loss component
+    fake_loss = criterion(disc_pred_fake , torch.zeros_like(disc_pred_fake))
+    # Get discriminator's predictions for real images
+    disc_pred_real = disc(real)
+    # Calculate the real loss component
+    real_loss = crieterion(disc_pred_real , torch.ones_like(disc_pred_real))
+    disc_loss = (real_loss + fake_loss) / 2
+    return disc_loss
+#########################################
+for epoch in range(1):
+    for real in dataloader:
+        cur_batch_size = len(real)
+        
+        disc_opt.zero_grad()
+        # Calculate discriminator loss
+        d_loss = disc_loss(gen , disc , real , cur_batch_size , 16)
+        # Compute gradients
+        d_loss.backward()
+        disc_opt.step()
+
+        gen_opt.zero_grad()
+        # Calculate generator loss
+        g_loss = gen_loss(gen , disc , cur_batch_size , 16)
+        # Compute generator gradients
+        g_loss.backward(g_loss)
+        gen_opt.step()
+
+        print(f"Generator loss: {g_loss}")
+        print(f"Discriminator loss: {d_loss}")
+        break
+##########################
+num_images_to_generate = 5
+# Create random noise tensor
+noise = torch.randn(num_images_to_generate , 16)
+
+# Generate images
+with torch.no_grad():
+    fake = gen(noise)
+print(f"Generated tensor shape: {fake.shape}")
+    
+for i in range(num_images_to_generate):
+    # Slice fake to select i-th image
+    image_tensor = fake[i , : , : , : ]
+    # Permute the image dimensions
+    image_tensor_permuted = image_tensor.permute(1 , 2, 0)
+    plt.imshow(image_tensor_permuted)
+    plt.show()
+#######################
+# Import FrechetInceptionDistance
+from torchmetrics.image.fid import FrechetInceptionDistance
+
+# Instantiate FID
+fid = FrechetInceptionDistance(feature = 64)
+
+# Update FID with real images
+fid.update((fake * 255).to(torch.uint8), real=False)
+fid.update((real * 255).to(torch.uint8) , real = True)
+
+# Compute the metric
+fid_score = fid.compute()
+print(fid_score)

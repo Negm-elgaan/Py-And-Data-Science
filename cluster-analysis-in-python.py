0e2aced84e8655f238919702354e325c67f52d51
Negm-elgaan <ohossam962@gmail.com>
2026-03-20 02:17:57 +0200

    Newww

--- cluster-analysis-in-python.py
+++ cluster-analysis-in-python.py
@@ -132,3 +132,34 @@ comic_con['cluster_labels'] = fcluster(distance_matrix , 2 , criterion = 'maxclu
 sns.scatterplot(x='x_scaled', y='y_scaled', 
                 hue='cluster_labels', data = comic_con)
 plt.show()
+#############################
+# Import the pyplot class
+import matplotlib.pyplot as plt
+
+# Define a colors dictionary for clusters
+colors = {1 : 'orange' , 2 : 'blue'}
+
+# Plot a scatter plot
+comic_con.plot.scatter(x='x_scaled', 
+                	   y='y_scaled',
+                	   c=comic_con['cluster_labels'].apply(lambda x : colors[x]))
+plt.show()
+######################################
+# Import the seaborn module
+import seaborn as sns
+
+# Plot a scatter plot using seaborn
+sns.scatterplot(x='x_scaled', 
+                y='y_scaled', 
+                hue='cluster_labels', 
+                data = comic_con)
+plt.show()
+#####################
+# Import the dendrogram function
+from scipy.cluster.hierarchy import dendrogram
+
+# Create a dendrogram
+dn = dendrogram(distance_matrix)
+
+# Display the dendogram
+plt.show()

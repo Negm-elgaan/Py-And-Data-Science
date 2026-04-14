2a1d8fc9d9b942b0fe1b20031bc14ae5ef346a61
Negm-elgaan <ohossam962@gmail.com>
2026-04-14 03:28:52 +0200

    Sentiment analysis yay

--- working-with-hugging-face.py
+++ working-with-hugging-face.py
@@ -7,3 +7,28 @@ results = gpt2_pipeline("What if AI", max_new_tokens=10, num_return_sequences=2)
 
 for result in results:
     print(result['generated_text'])
+######################
+from datasets import load_dataset
+# Load the "validation" split of the TIGER-Lab/MMLU-Pro dataset
+my_dataset = load_dataset("TIGER-Lab/MMLU-Pro" , split = 'validation')
+
+# Display dataset details
+print(my_dataset)
+###############################
+# Filter the documents
+filtered = wikipedia.filter(lambda row: "football" in row["text"])
+
+# Create a sample dataset
+example = filtered.select(range(1))
+
+print(example[0]["text"])
+##################################################
+# Create a pipeline for grammar checking
+grammar_checker = pipeline(
+  task="text-classification", 
+  model="abdulmatinomotoso/English_Grammar_Checker"
+)
+
+# Check grammar of the input text
+output = grammar_checker("I will walk dog")
+print(output)

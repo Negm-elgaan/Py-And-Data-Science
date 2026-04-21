e731752abb830fa482cb30558ffb0723490de0ce
Negm-elgaan <ohossam962@gmail.com>
2026-04-21 19:23:38 +0200

    More and more

---
+++ arima-models-in-python.py
@@ -0,0 +1,13 @@
+# Import modules
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load in the time series
+candy = pd.read_csv('candy_production.csv', 
+            index_col='date',
+            parse_dates=True)
+
+# Plot and show the time series on axis ax1
+fig, ax1 = plt.subplots()
+candy.plot(ax=ax1)
+plt.show()

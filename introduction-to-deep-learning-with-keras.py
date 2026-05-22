3b253632ceefec17b7ec47cd995c11a910b8f6b7
Negm-elgaan <ohossam962@gmail.com>
2026-05-22 22:54:03 +0300

    mnczsja

--- introduction-to-deep-learning-with-keras.py
+++ introduction-to-deep-learning-with-keras.py
@@ -319,3 +319,18 @@ h_callback = model.fit(X_train , y_train , epochs = 60 ,  validation_data = (X_t
 
 # Extract from the h_callback object loss and val_loss to plot the learning curve
 plot_loss(h_callback.history['loss'], h_callback.history['val_loss'])
+##########################################
+for size in training_sizes:
+  	# Get a fraction of training data (we only care about the training data)
+    X_train_frac, y_train_frac = X_train[:size], y_train[:size]
+
+    # Reset the model to the initial weights and train it on the new training data fraction
+    model.set_weights(initial_weights)
+    model.fit(X_train_frac, y_train_frac, epochs = 50, callbacks = [early_stop])
+
+    # Evaluate and store both: the training data fraction and the complete test set results
+    train_accs.append(model.evaluate(X_train_frac, y_train_frac)[1])
+    test_accs.append(model.evaluate(X_test, y_test)[1])
+    
+# Plot train vs test accuracies
+plot_results(train_accs, test_accs)

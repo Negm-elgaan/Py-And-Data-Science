110096fdf8f21202bed40e2e95191b559f9e6c91
Negm-elgaan <ohossam962@gmail.com>
2026-06-09 20:39:54 +0300

    Keras first course is done!

--- introduction-to-deep-learning-with-keras.py
+++ introduction-to-deep-learning-with-keras.py
@@ -471,3 +471,107 @@ decoded_imgs = autoencoder.predict(X_test_noise)
 
 # Plot noisy vs decoded images
 compare_plot(X_test_noise, decoded_imgs)
+#######################################
+# Import the Conv2D and Flatten layers and instantiate model
+from tensorflow.keras.layers import Conv2D , Flatten
+model = Sequential()
+
+# Add a convolutional layer of 32 filters of size 3x3
+model.add(Conv2D(32, kernel_size = 3, input_shape = (28, 28, 1), activation = 'relu'))
+
+# Add a convolutional layer of 16 filters of size 3x3
+model.add(Conv2D(16, kernel_size = 3, activation = 'relu'))
+
+# Flatten the previous layer output
+model.add(Flatten())
+
+# Add as many outputs as classes with softmax activation
+model.add(Dense(10, activation = 'softmax'))
+############################################
+# Obtain a reference to the outputs of the first layer
+first_layer_output = model.layers[0].output
+
+# Build a model using the model's input and the first layer output
+first_layer_model = Model(inputs = model.layers[0].input, outputs = first_layer_output)
+
+# Use this model to predict on X_test
+activations = first_layer_model.predict(X_test)
+
+# Plot the activations of first digit of X_test for the 15th filter
+axs[0].matshow(activations[0,:,:,14], cmap = 'viridis')
+
+# Do the same but for the 18th filter now
+axs[1].matshow(activations[0,:,:,17], cmap = 'viridis')
+plt.show()
+#######################################################################################
+# Import image and preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input
+
+# Load the image with the right target size for your model
+img = image.load_img(img_path, target_size=(224, 224))
+
+# Turn it into an array
+img_array = image.img_to_array(img)
+
+# Expand the dimensions of the image, this is so that it fits the expected model input format
+img_expanded = np.expand_dims(img_array, axis = 0)
+
+# Pre-process the img in the same way original images were
+img_ready = preprocess_input(img_expanded)
+###########################################
+# Instantiate a ResNet50 model with 'imagenet' weights
+model = ResNet50(weights='imagenet')
+
+# Predict with ResNet50 on your already processed img
+preds = model.predict(img_ready)
+
+# Decode the first 3 predictions
+print('Predicted:', decode_predictions(preds, top=3)[0])
+####################################################
+# Split text into an array of words 
+words = text.split()
+
+# Make sentences of 4 words each, moving one word at a time
+sentences = []
+for i in range(4, len(words)):
+  sentences.append(' '.join(words[i-4:i]))
+
+# Instantiate a Tokenizer, then fit it on the sentences
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sentences)
+
+# Turn sentences into a sequence of numbers
+sequences = tokenizer.texts_to_sequences(sentences)
+print("Sentences: \n {} \n Sequences: \n {}".format(sentences[:5],sequences[:5]))
+#####################################################################
+# Import the Embedding, LSTM and Dense layer
+from tensorflow.keras.layers import Embedding , LSTM , Dense
+
+model = Sequential()
+
+# Add an Embedding layer with the right parameters
+model.add(Embedding(input_dim = vocab_size , input_length = 3 , output_dim = 8 , ))
+
+# Add a 32 unit LSTM layer
+model.add(LSTM(32))
+
+# Add a hidden Dense layer of 32 units and an output layer of vocab_size with softmax
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='softmax'))
+model.summary()
+###########################
+def predict_text(test_text, model = model):
+  if len(test_text.split()) != 3:
+    print('Text input should be 3 words!')
+    return False
+  
+  # Turn the test_text into a sequence of numbers
+  test_seq = tokenizer.texts_to_sequences([test_text])
+  test_seq = np.array(test_seq)
+  
+  # Use the model passed as a parameter to predict the next word
+  pred = model.predict(test_seq).argmax(axis = 1)[0]
+  
+  # Return the word that maps to the prediction
+  return tokenizer.index_word[pred]

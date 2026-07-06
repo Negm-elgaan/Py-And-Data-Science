1bb2ce1fe425087f77f2e4203635a67cdf28695e
Negm-elgaan <ohossam962@gmail.com>
2026-07-06 23:40:37 +0300

    kjtgvjkjiogytewardtfygui

--- deep-learning-for-text-with-pytorch.py
+++ deep-learning-for-text-with-pytorch.py
@@ -40,3 +40,41 @@ filtered_tokens = [token for token in tokens if token.lower() not in stop_words]
 stemmer = PorterStemmer()
 stemmed_tokens = [stemmer.stem(token) for token in filtered_tokens]
 print(stemmed_tokens)
+##################################################
+genres = ['Fiction','Non-fiction','Biography', 'Children','Mystery']
+
+# Define the size of the vocabulary
+vocab_size = len(genres)
+
+# Create one-hot vectors
+one_hot_vectors = torch.eye(vocab_size)
+
+# Create a dictionary mapping genres to their one-hot vectors
+one_hot_dict = {genre : one_hot_vectors[i] for i, genre in enumerate(genres)}
+
+for genre, vector in one_hot_dict.items():
+    print(f'{genre}: {vector.numpy()}')
+#############################################################
+# Import from sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+
+titles = ['The Great Gatsby','To Kill a Mockingbird','1984','The Catcher in the Rye','The Hobbit', 'Great Expectations']
+
+# Initialize Bag-of-words with the list of book titles
+vectorizer = CountVectorizer()
+bow_encoded_titles = vectorizer.fit_transform(titles)
+
+# Extract and print the first five features
+print(vectorizer.get_feature_names_out()[:5])
+print(bow_encoded_titles.toarray()[0, :5])
+#####################################################################
+# Importing TF-IDF from sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Initialize TF-IDF encoding vectorizer
+vectorizer = TfidfVectorizer()
+tfidf_encoded_descriptions = vectorizer.fit_transform(descriptions)
+
+# Extract and print the first five features
+print(vectorizer.get_feature_names_out()[:5])
+print(tfidf_encoded_descriptions.toarray()[0, :5])
--- introduction_to_MongoDB_in_Python.py
+++ introduction_to_MongoDB_in_Python.py
@@ -2,3 +2,12 @@
 from pymongo import MongoClient as MC
 # Create client
 client = MC()
+################################
+from pymongo import MongoClient 
+client = MongoClient()
+
+# Print out the names of all databases in your server
+print(client.list_database_names())
+
+# Print out the names of all collection in the film database
+print(client.film.list_collection_names())

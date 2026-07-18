bde168064d5e00c5ba4903748f2a5240ee23e450
Negm-elgaan <ohossam962@gmail.com>
2026-07-18 07:50:03 +0300

    COMMIT 19183782938

--- spoken-language-processing-in-python.py
+++ spoken-language-processing-in-python.py
@@ -8,3 +8,29 @@ signal_gm = good_morning.readframes(-1)
 
 # View first 10
 print(signal_gm[:10])
+###################################
+import numpy as np
+
+# Open good morning sound wave and read frames as bytes
+good_morning = wave.open("good_morning.wav", 'r')
+signal_gm = good_morning.readframes(-1)
+
+# Convert good morning audio bytes to integers
+soundwave_gm = np.frombuffer(signal_gm , dtype = "int16")
+
+# View the first 10 sound wave values
+print(soundwave_gm[:10])
+##########################################################
+# Read in sound wave and convert from bytes to integers
+good_morning = wave.open('good_morning.wav', 'r')
+signal_gm = good_morning.readframes(-1)
+soundwave_gm = np.frombuffer(signal_gm, dtype='int16')
+
+# Get the sound wave frame rate
+framerate_gm = good_morning.getframerate()
+
+# Find the sound wave timestamps
+time_gm = np.linspace(start = 0 , stop = len(soundwave_gm) / framerate_gm , num = len(soundwave_gm))
+
+# Print the first 10 timestamps
+print(time_gm[:10])

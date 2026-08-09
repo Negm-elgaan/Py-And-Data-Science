7223536d5bddbf7f660ef14f0e7153d1dd9a308b
Negm-elgaan <ohossam962@gmail.com>
2026-08-09 23:55:25 +0300

    Daily2

--- introduction-to-numpy.py
+++ introduction-to-numpy.py
@@ -317,3 +317,29 @@ rgb_array = np.load("mystery_image.npy")
 
 plt.imshow(rgb_array)
 plt.show()
+#############################
+# Display the documentation for .astype()
+help(np.ndarray.astype)
+######################################
+# Reduce every value in rgb_array by 50 percent
+darker_rgb_array = rgb_array * 0.5
+################################################
+# Reduce every value in rgb_array by 50 percent
+darker_rgb_array = rgb_array * 0.5
+
+# Convert darker_rgb_array into an array of integers
+darker_rgb_int_array = darker_rgb_array.astype(int)
+plt.imshow(darker_rgb_int_array)
+plt.show()
+#################################
+# Reduce every value in rgb_array by 50 percent
+darker_rgb_array = rgb_array * 0.5
+
+# Convert darker_rgb_array into an array of integers
+darker_rgb_int_array = darker_rgb_array.astype(np.int8)
+plt.imshow(darker_rgb_int_array)
+plt.show()
+
+# Save darker_rgb_int_array to an .npy file called darker_monet.npy
+with open("darker_monet.npy" , "wb") as f:
+    np.save(f , darker_rgb_int_array)

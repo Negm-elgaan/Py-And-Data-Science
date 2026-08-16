5e21e8a0808f77a0cf8ec5c016984f532199b736
Negm-elgaan <ohossam962@gmail.com>
2026-08-16 09:07:43 +0300

    Final numpy

--- introduction-to-numpy.py
+++ introduction-to-numpy.py
@@ -343,3 +343,78 @@ plt.show()
 # Save darker_rgb_int_array to an .npy file called darker_monet.npy
 with open("darker_monet.npy" , "wb") as f:
     np.save(f , darker_rgb_int_array)
+#############################################
+# Flip rgb_array so that it is the mirror image of the original
+mirrored_monet = np.flip(rgb_array , axis = (1))
+plt.imshow(mirrored_monet)
+plt.show()
+######################################################
+# Flip rgb_array so that it is upside down
+upside_down_monet = np.flip(rgb_array , axis = (0 , 1) ) 
+plt.imshow(upside_down_monet)
+plt.show()
+###################################
+# Transpose rgb_array
+transposed_rgb = np.transpose(rgb_array , axes = (1  , 0  ,  2))
+plt.imshow(transposed_rgb)
+plt.show()
+################################################
+# Split monthly_sales into quarterly data
+q1_sales, q2_sales, q3_sales, q4_sales = np.split(monthly_sales , 4)
+print(q1_sales)
+####################################
+# Split monthly_sales into quarterly data
+q1_sales, q2_sales, q3_sales, q4_sales = np.split(monthly_sales, 4)
+
+# Print q1_sales
+print(q1_sales)
+
+# Stack the four quarterly sales arrays
+quarterly_sales = np.stack([q1_sales, q2_sales, q3_sales, q4_sales])
+print(quarterly_sales)
+######################################################################
+# Split rgb_array into red, green, and blue arrays
+red_array, green_array, blue_array = np.split(rgb_array , 3 , axis = 2)
+#############################################
+# Split rgb_array into red, green, and blue arrays
+red_array, green_array, blue_array = np.split(rgb_array, 3, axis=2)
+
+# Create emphasized_blue_array
+emphasized_blue_array = np.where(blue_array > blue_array.mean() , 255 , blue_array)
+
+# Print the shape of emphasized_blue_array
+print(emphasized_blue_array.shape)
+###########################################
+# Split rgb_array into red, green, and blue arrays
+red_array, green_array, blue_array = np.split(rgb_array, 3, axis=2)
+
+# Create emphasized_blue_array
+emphasized_blue_array = np.where(blue_array > blue_array.mean(), 255, blue_array)
+
+# Print the shape of emphasized_blue_array
+print(emphasized_blue_array.shape)
+
+# Remove the trailing dimension from emphasized_blue_array
+emphasized_blue_array_2D = emphasized_blue_array.reshape(675 , 844)
+###########################
+# Print the shapes of blue_array and emphasized_blue_array_2D
+print(blue_array.shape , emphasized_blue_array_2D.shape)
+##############################################################
+# Print the shapes of blue_array and emphasized_blue_array_2D
+print(blue_array.shape, emphasized_blue_array_2D.shape)
+
+# Reshape red_array and green_array
+red_array_2D = red_array.reshape(675 , 844)
+green_array_2D = green_array.reshape(675 , 844)
+##################################################################################################################
+# Print the shapes of blue_array and emphasized_blue_array_2D
+print(blue_array.shape, emphasized_blue_array_2D.shape)
+
+# Reshape red_array and green_array
+red_array_2D = red_array.reshape((675, 844))
+green_array_2D = green_array.reshape((675, 844))
+
+# Stack red_array_2D, green_array_2D, and emphasized_blue_array_2D
+emphasized_blue_monet = np.stack([red_array_2D , green_array_2D , emphasized_blue_array_2D] , axis = 2)
+plt.imshow(emphasized_blue_monet)
+plt.show()

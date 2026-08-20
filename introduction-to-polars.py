93b9dbe595db78896b413ecd9497348b772d783e
Negm-elgaan <ohossam962@gmail.com>
2026-08-20 14:56:33 +0300

    Chapter 1 polars is done

--- introduction-to-polars.py
+++ introduction-to-polars.py
@@ -25,3 +25,27 @@ print(ev_df[['brand' , 'model' , 'price']])
 ###############################################
 # Print the last three rows of the brand and price columns
 print(ev_df[-3 : , ['brand' , 'price']])
+########################
+# Select the model, accel and price columns
+print(ev_df.select(["model" , "accel" , "price"]))
+######################################
+# Select the brand, model and price columns
+print(ev_df.select(["brand" , "model" , "price"]))
+############################################
+# Sort with longest range first
+sorted_ev_df = ev_df.sort("range" , descending = True)
+
+print(sorted_ev_df)
+###################################
+# Print a summary of the DataFrame
+print(ev_df.describe())
+###############################
+# Get the most expensive 4 vehicles
+expensive_df = ev_df.top_k(4 , by = "price")
+
+print(expensive_df)
+###########################################
+# Find the four cheapest vehicles
+cheap_df = ev_df.bottom_k(4  ,  by = "price")
+
+print(cheap_df)
